6ac84db34229b62408102b85291ce79c629f5396
min0 <kminy0128@naver.com>
2024-06-28 17:02:56 +0900

    Add Module

--- custom/cls.c
+++ custom/cls.c
@@ -5,59 +5,116 @@
 #include <linux/errno.h>
 #include <linux/fs.h>
 
-int CLS_MAJOR= 0; // save the major number of the device
+#define DEV_NAME "cls"
+#define DEV_CAPACITY 1024*1024 // 1MB
+#define QUEUE_LIMIT 16
 
-int cls_open(struct inode *inode, struct file *filp)
+static int CLS_MAJOR= 0; // save the major number of the device
+
+/*typedef struct block_dev{
+	sector_t capacity;
+	unsigned char *data;
+	struct gendisk gdisk*;
+} block_dev_t;*/
+
+struct queue_limits lim = {
+	.max_sectors = 64
+};
+
+
+
+static void cls_request(struct request_queue *q)
+{
+	printk("CLS get request!");
+	return;
+}
+
+
+
+static int cls_open(struct gendisk *gdisk, fmode_t mode)
 {
 	printk("CLS Device Drive open !\n");
 	return 0;
 }
 
-int cls_release(struct inode *inode, struct file *filp)
+static void cls_release(struct gendisk *gd)
 {
 	printk("CLS Device Drive released!\n");
-	return 0;
-}
-
-int cls_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg )
-{
-	printk("CLS Device Drive ioctl!\n");
-	return 0;
 }
 
 
 
-static struct file_operations cls_fileops = {
+
+static struct block_device_operations cls_fops = {
+	.owner = THIS_MODULE,
 	.open = cls_open,
 	.release = cls_release,
 };
 
-int cls_init(void)
+/*static int blkdev_add_device(void){
+	struct gendisk *disk;
+
+	block_dev_t *dev = kzalloc(sizeof(block_dev_t), GFP_KERNEL);
+
+	if(dev == NULL){
+		prinkt(KERN_WARNING "CLS: kzalloc Error");
+		return -1;
+	}
+
+	cls_dev = dev;
+
+	if((disk = alloc_disk(1)) == NULL){
+		prinkt(KERN_WARNING "CLS: disk alloc Error");
+		return -1;
+	}
+
+	disk->
+}*/
+static int __init cls_init(void)
 {
 	int result;
-
-	result = register_blkdev(CLS_MAJOR, "csl");
+	
+	static struct gendisk *gdisk;
+	result = register_blkdev(CLS_MAJOR, "cls");
 	
 	// error handling
-	if(result < 0)
-	{
+	if(result < 0)	{
 		printk(KERN_WARNING "CLS: Fail to get major number!\n");
 		return result;
 	}
 
-	if( CLS_MAJOR == 0){
+	if(CLS_MAJOR == 0){
 		CLS_MAJOR = result;
 	}
 	
+	
+	gdisk = blk_alloc_disk(&lim,NUMA_NO_NODE);
+	if(!gdisk){
+		printk(KERN_WARNING "CLS: Fail to allocate disk!\n");
+		return -1;
+	}
+	
+	sprintf(gdisk->disk_name,DEV_NAME);
+	
+	gdisk->major = CLS_MAJOR;
+	gdisk->fops = &cls_fops;
+	gdisk->first_minor = 0;
+	set_capacity(gdisk, DEV_CAPACITY);
+	
+	int i = add_disk(gdisk);
+	if(!i){
+		printk(KERN_WARNING "CLS: Fail to add disk!\n");
+		return -1;
+	}
 	printk(KERN_INFO "DEVICE : CLS is successfully initialized with major number %d\n",CLS_MAJOR);
 	return 0;
 }
 
 
 
-void cls_exit(void)
+static void __exit cls_exit(void)
 {
-	unregister_blkdev(CLS_MAJOR,"cls");
+	unregister_blkdev(CLS_MAJOR,DEV_NAME);
 	printk(KERN_INFO "DEVICE : CLS is successfully unregistered!\n");
 	
 }
@@ -67,4 +124,4 @@ module_exit(cls_exit);
 
 MODULE_AUTHOR("Minyoung   kim");
 MODULE_DESCRIPTION("Virtual Block Device Driver");
-MODULE_LICENSE("@min0");
+MODULE_LICENSE("GPL");
